a83c943f5da77b0ff054078968ecadd3ee031ba9
Folkes <samfolke@08f8bc67955b.ant.amazon.com>
2020-12-04 03:50:35 -0600

    fixed artifact name

--- src/main/java/com/oktank/demo/Application.java
+++ src/main/java/com/oktank/demo/Application.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import com.oktank.demo.controller.PingController;
+import com.oktank.demo.controller.PetsController;
 
 
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "com.oktank.demo.controller")
-@Import({ PingController.class })
+@Import({ PetsController.class })
 public class Application {
 
     public static void main(String[] args) {
