e8160c9de69788a9c13720f5a447fb8093c2b481
Folkes <samfolke@08f8bc67955b.ant.amazon.com>
2021-01-05 14:25:22 -0600

    added update

--- src/util/verify/verify.py
+++ src/util/verify/verify.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import os
 from botocore.exceptions import ClientError
 
 sqs = boto3.client('sqs')
@@ -19,8 +20,9 @@ sqs = boto3.client('sqs')
 def lambda_handler(message, context):
     print(json.dumps(message))
     verification_id = json.loads(message["Records"][0]["body"])["id"]
-    id = json.loads(message["Records"][0]["body"])["idPhoto"]
-    image = 'verifications/'+json.loads(message["Records"][0]["body"])["idPhoto"]
+    id = json.loads(message["Records"][0]["body"])["id"]
+    name = json.loads(message["Records"][0]["body"])["name"]
+    image = 'verifications/'+json.loads(message["Records"][0]["body"])["id"]+'.jpg'
     print("id:",id,"image:",image)
     client = boto3.client('rekognition')
     verified = False
@@ -44,6 +46,7 @@ def lambda_handler(message, context):
         )
         if len(rekognitionResponse["FaceMatches"]) > 0:
             verified = True
+            updateRecord(id)
         else:
             verified = False
     except ClientError as e:
@@ -52,12 +55,25 @@ def lambda_handler(message, context):
     sqs_url = os.environ['NOTIFY_QUEUE_URL']
     response = sqs.send_message(
         QueueUrl=sqs_url,
-        MessageBody=(json.dumps({'message_type':'response','entity_id':'OKTANK','requester_email':'samfolke@amazon.com','subject_name':'Samuel Folkes','verification_id': verification_id, 'verified':verified}))
+        MessageBody=(json.dumps({'message_type':'response','entity_id':'OKTANK','requester_email':'samfolke@amazon.com','subject_name':name,'verification_id': verification_id, 'verified':verified}))
     )
     print(json.dumps((json.dumps({'verification_id': verification_id, 'verified':verified}))))
     return {
         'statusCode': 200,
         'body':'success'
     }
+
+def updateRecord(id):
+    rds = boto3.client('rds-data')
+    cluster_arn = os.environ['DB_CLUSTER_ARN']
+    secret_arn = os.environ['SECRET_ARN']
+
+    result = rds.execute_statement(
+        resourceArn = cluster_arn,
+        secretArn = secret_arn,
+        database = 'oktankdemo',
+        sql = "update employees set verified = true where id ='"+id+"'")
+
+    print(result)
 
 
